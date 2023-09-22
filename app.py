d86589b38b4b7b3495420257cef9516f2d3a99e8
Qiu1117 <995410710@qq.com>
2023-09-22 17:42:17 +0800

    web-page & user-page

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-from flask import Flask, request, render_template, jsonify, redirect, session
+from flask import Flask, request, jsonify, send_file
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    decode_token,
+    jwt_required,
+    get_jwt_identity,
+)
+from jwt.exceptions import ExpiredSignatureError
+from flask_cors import CORS
 from pymongo import MongoClient
 import pydicom
+from bson.objectid import ObjectId
 import os
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
+CORS(app)
+jwt = JWTManager(app)
 app.secret_key = "cuhkdiir"
 client = MongoClient("mongodb://localhost:27017")
 db = client["Dicom"]
 
 UPLOAD_FOLDER = r"C:\Users\Qiuyi\Desktop\uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-
-
-@app.route("/")
-def index():
-    if "username" not in session:
-        return redirect("/login")
-    else:
-        return render_template("index.html")
 
 
 @app.route("/upload", methods=["POST"])
@@ -85,71 +89,84 @@ def search_data_by_ip():
     return jsonify(res)
 
 
+@app.route("/get_file", methods=["GET"])
+def get_file():
+    file_address = request.args.get("fileAddress")
+
+    # 根据文件地址发送文件内容作为响应
+    return send_file(file_address)
+
+
 # ---------------------------------------用户和注册--------------------------
 users_collection = db["users"]
 
+# # 注销
+# @app.route("/logout")
+# def logout():
+#     # 清除用户登录状态
+#     session.pop("username", None)
+#     return redirect("/login")
 
-# 注册页面
-@app.route("/register", methods=["GET", "POST"])
+
+@app.route("/register", methods=["POST"])
 def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+    username = request.json["username"]
+    password = request.json["password"]
 
-        # 检查用户名是否已存在
-        existing_user = users_collection.find_one({"username": username})
-        if existing_user:
-            return "Username already exists! Please choose a different username."
+    # 检查用户名是否已存在
+    existing_user = users_collection.find_one({"username": username})
+    if existing_user:
+        return jsonify({"message": "Username already exists"}), 400
 
-        # 创建新用户
-        hashed_password = generate_password_hash(password)
-        new_user = {"username": username, "password": hashed_password}
-        users_collection.insert_one(new_user)
+    # 创建新用户
+    hashed_password = generate_password_hash(password)
+    user_id = users_collection.insert_one(
+        {"username": username, "password": hashed_password}
+    ).inserted_id
 
-        return redirect("/login")
-
-    return render_template("register.html")
+    return jsonify({"user_id": str(user_id)}), 201
 
 
-# 登录页面
-@app.route("/login", methods=["GET", "POST"])
+# 用户登录接口
+@app.route("/login", methods=["POST"])
 def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+    username = request.json["username"]
+    password = request.json["password"]
 
-        # 根据用户名查找用户
-        user = users_collection.find_one({"username": username})
-        if not user or not check_password_hash(user["password"], password):
-            return "Invalid username or password."
+    # 根据用户名查找用户
+    user = users_collection.find_one({"username": username})
 
-        session["username"] = username
+    if user and check_password_hash(user["password"], password):
+        # 生成访问令牌
+        access_token = create_access_token(identity=str(user["_id"]))
+        return jsonify({"access_token": access_token})
 
-        return redirect("/dashboard")
-
-    return render_template("login.html")
+    return jsonify({"message": "Invalid username or password"}), 401
 
 
-# 用户仪表盘页面
-@app.route("/dashboard")
-def dashboard():
-    # 检查用户是否已登录
-    if "username" not in session:
-        return redirect("/login")
+@app.route("/verify-token", methods=["POST"])
+def verify_token():
+    token = request.json.get("token")
+    try:
+        decoded_token = decode_token(token)
+        user_id = decoded_token["sub"]
+        user = users_collection.find_one({"_id": ObjectId(user_id)})
+        if user:
+            return jsonify({"valid": True, "sub": user_id})
 
-    username = session["username"]
-    return render_template("index.html", username=username)
-
-
-# 注销
-@app.route("/logout")
-def logout():
-    # 清除用户登录状态
-    session.pop("username", None)
-    return redirect("/login")
+    except ExpiredSignatureError:
+        return jsonify({"valid": False, "message": "Token has expired"})
+    except Exception as e:
+        return jsonify({"valid": False, "message": "Invalid token"})
 
 
 # ---------------------------------------用户和注册--------------------------
+
+
+# @app.route("/", defaults={"path": ""})
+# @app.route("/<path:path>")
+# def catch_allroute(path):
+#     return render_template("index.html")
 
 
 if __name__ == "__main__":
